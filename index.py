22758efc124ce98a151beb1dd7f1efd005b98a30
monnku <129637317+monnku@users.noreply.github.com>
2024-05-15 07:14:31 +0900

    Update index.py

--- index.py
+++ index.py
@@ -4,13 +4,13 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def form():
-    return render_template('apps/app.html')
+    return render_template('templates/index.html')
 
 @app.route('/data', methods=['POST'])
 def do():
     user = request.form['username']
     pw = request.form['password']
-    return render_template('apps/app.html')
+    return render_template('templates/index.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
