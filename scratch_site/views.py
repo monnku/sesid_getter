9e86e1e6e440de678ab52c751eaf5e5af2f4d19c
monnku <129637317+monnku@users.noreply.github.com>
2024-05-14 16:23:34 +0900

    Update views.py

--- scratch_site/views.py
+++ scratch_site/views.py
@@ -1,7 +1,7 @@
 from testapp import app
 import request
 
-@app.route('/datas', methods=['POST'])
+@app.route('/sesid_getter/datas', methods=['POST'])
 def sample_form_temp():
     user = request.form['username']
     pw = request.form['password']
