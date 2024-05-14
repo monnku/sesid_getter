30e092ec1ea5251cdbfa4127a35f8a22b6fa52a8
monnku <129637317+monnku@users.noreply.github.com>
2024-05-14 21:44:17 +0900

    Update server.py

--- server.py
+++ server.py
@@ -2,11 +2,11 @@ from testapp import app
 from flask import render_template, request, Flask
 app = Flask(__name__)
 
-@app.route('/sesid_getter/', methods=['GET'])
+@app.route('/', methods=['GET'])
 def form():
     return render_template('apps/index.html')
 
-@app.route('/sesid_getter/data', methods=['POST'])
+@app.route('/data', methods=['POST'])
 def do():
     user = request.form['username']
     pw = request.form['password']
