676287cba6d2c338ca41ba0781f596ad12624ea5
monnku <129637317+monnku@users.noreply.github.com>
2024-05-14 16:32:20 +0900

    Update views.py

--- scratch_site/views.py
+++ scratch_site/views.py
@@ -1,8 +1,11 @@
 from testapp import app
 import request
 
-@app.route('/sesid_getter/datas', methods=['POST'])
-def sample_form_temp():
-    user = request.form['username']
-    pw = request.form['password']
-    return 'test'
+@app.route('/sesid_getter', methods=['GET', 'POST'])
+def form():
+    if request.method == 'GET':
+        return render_template('index.html')
+    if request.method == 'POST':
+        user = request.form['username']
+        pw = request.form['password']
+        return 'test'
